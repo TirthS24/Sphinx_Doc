5d9b426fb05cf0df02b65c3a60bfe07a325927cd
Tirth Shah <tirths@york.ie>
2024-12-26 15:54:03 +0530

    chore: Use of buil-in extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,9 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.duration',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -26,3 +28,6 @@ exclude_patterns = []
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
+# EPUB options
+epub_show_urls = 'footnote'
