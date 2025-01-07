70b0fc0a7a31411c5219feafb9930a48f42c7ba2
Tirth Shah <tirths@york.ie>
2025-01-07 19:04:00 +0530

    add: docs folder and configuration file added

---
+++ docs/conf.py
@@ -0,0 +1,39 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Sphinx Auto-Doc'
+copyright = '2025, Tirth Shah'
+author = 'Tirth Shah'
+release = '0.0.1'
+
+# -- Initial Configuration ---------------------------------------------------
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../src'))
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.viewcode',
+    'sphinx.ext.autodoc',
+    'sphinx_js',
+    'sphinx_graphiql'
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/modules.rst
@@ -0,0 +1,8 @@
+src
+===
+
+.. toctree::
+   :maxdepth: 4
+
+   shapes_modules
+   sweets_modules
